13df6a239628e92bf7e3da11d50afac6436d38de
Abdul Wahab <138891037+wahab-277@users.noreply.github.com>
2024-10-02 09:31:10 +0500

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,16 @@
+
+import React from "react";
+import Header from "./components/header";
+import Footer from "./components/footer";
+
+
+export default function Home() {
+  return (
+    <div>
+    <Header />
+    <h1 id="hme">This is Home page</h1>
+    <Footer />
+    </div>
+
+  );
+}
